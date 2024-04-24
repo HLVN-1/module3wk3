61b6b0eb6265cdd0a50e51899575c855bb9f8817
Jason Newsom <jnewsom405@gmail.com>
2024-04-24 18:29:29 -0500

    did a web request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs, { access } from 'fs';
 import _ from 'lodash';
-import { color, log, red, green } from 'console-log-colors';
+import { color, log, red, green, blue } from 'console-log-colors';
+import request from 'request';
 
 const myArray = [ 1, 2, 3, 4, 5, 8, 4, 3, 9]
 console.log(color.red(myArray));
@@ -20,3 +21,11 @@ console.log(file);
 
 // Asynchronous File Access method
 fs.readFile('./data.json', {encoding: 'utf-8'}, (err, f) => console.log(color.green(f)));
+
+// web request
+
+request('https://jsonplaceholder.typicode.com/posts', (err, resp, body) => {
+    const parsed = JSON.parse(body);
+    parsed.forEach( (x) => console.log(x));
+    // console.log(color.blue(parsed))
+});
