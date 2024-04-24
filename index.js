a044216d62a74710d97d1faf9bf5184cefe515ae
Jason Newsom <jnewsom405@gmail.com>
2024-04-24 18:20:53 -0500

    learned file system stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs, { access } from 'fs';
 import _ from 'lodash';
 import { color, log, red, green } from 'console-log-colors';
 
@@ -12,3 +13,10 @@ console.log(output);
 const backwards = _.sortBy(myArray, (a,b) => b < a)
 
 console.log(color.green(backwards));
+
+// Synchronous File access
+const file = fs.readFileSync('./data.json', {encoding: 'utf-8'});
+console.log(file);
+
+// Asynchronous File Access method
+fs.readFile('./data.json', {encoding: 'utf-8'}, (err, f) => console.log(color.green(f)));
